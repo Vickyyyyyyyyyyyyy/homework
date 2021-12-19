ead7f014a3697e9347c894daf444e0cfba085aa7
Vickyyyyyyyyyyyyy <93123748+Vickyyyyyyyyyyyyy@users.noreply.github.com>
2021-12-19 23:04:40 +0300

    Update main.cpp

--- homework2/main.cpp
+++ homework2/main.cpp
@@ -1,173 +1,98 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
-#include <iterator>
 #include <cmath>
-#include <algorithm>
 #include <vector>
-#include <string>
-#include <map>
 
-const double EarthGravity = 9.8;
+using namespace std;
 
+const double G = -9.8;
+typedef vector<pair<double, double>> Walls;
 
-typedef std::map<double, double> Walls;
-typedef std::map<double, int> Interval;
-
-
-typedef struct Velocity {
-	double v_x;
-	double v_y;
-};
-
-class Ball {
+class Trajectory {
 public:
-	Ball() {}
-	Ball(const std::string& filename);
 
-	std::ifstream file;
-	~Ball() { file.close(); }
+    Trajectory(string filename);
 
-	Walls walls;
-	Interval interval;
-	Velocity velocity;
+    pair<double, double> velocity;
+    Walls walls_;
+    double h;
+    double x = 0;
 
-	bool isInInterval();
-	void setVelocity();
-	void BuildWalls();
-	void KudaUpal();
+    int interval = 0;
+    int fall_interval;
+    int size = 0;
 
-private:
-	double h0;
-	double x0;
+    int findInterval(std::ifstream& infile);
 
-	double ymove, wall_x, wall_y, number_of_interval;
-
-	inline double time(double x, double x0) { return (x - x0) / velocity.v_x; }
-	inline double xMove(double t) { return x0 + velocity.v_x * t; }
-	inline double yMove(double t) { return h0 + velocity.v_y * t - (EarthGravity * t * t) / 2; }
 };
 
-Ball::Ball(const std::string& filename) {
-	x0 = 0;
-	number_of_interval = 0;
-	file.open(filename);
-	setVelocity();
-	BuildWalls();
+Trajectory::Trajectory(string filename) {
+
+    std::ifstream infile(filename);
+    infile >> h;
+    infile >> velocity.first >> velocity.second;
+
+    fall_interval = findInterval(infile);
 }
 
-bool Ball::isInInterval()
-{
-	return true;
+int Trajectory::findInterval(std::ifstream& infile) {
+
+    while (1) {
+        int target = 0;
+        
+        if (velocity.first < 0) {
+            target = interval - 1;
+            return target;
+        }
+
+        if (velocity.first > 0) {
+            target = interval;
+        }
+
+        if (target > size - 1) {
+            double wall_x, wall_h;
+            if (infile >> wall_x >> wall_h) {
+                walls_.push_back(std::make_pair(wall_x, wall_h));
+                size++;
+            }
+
+            else {
+                return size;
+            }
+
+        }
+
+        double t = fabs((x - walls_[target].first) / velocity.first);
+
+        x = walls_[target].first;
+        h = h + velocity.second * t + (G * t * t) / 2.0;
+        velocity.second = velocity.second + G * t;
+
+        if (h <= 0) {
+            return interval;
+        }
+
+        if (h > walls_[target].second) {
+            if (velocity.first < 0) {
+                interval--;
+            }
+
+            if (velocity.first > 0) {
+                interval++;
+            }
+        }
+
+        else {
+            velocity.first *= -1;
+        }
+    }
 }
 
-void Ball::setVelocity() {
-	file >> h0;
-	file >> velocity.v_x >> velocity.v_y;
-	std::cout << "v_x v_y set: " << velocity.v_x << " " << velocity.v_y << std::endl;
-}
 
-void Ball::BuildWalls() {
-	double tmpx, tmpy;
-	walls.insert(std::make_pair(-1, -1)); 
-	int i = 0;
-	do {
-		file >> tmpx >> tmpy;
-		walls.insert(std::make_pair(tmpx, tmpy));
-		interval.insert(std::make_pair(tmpx, i));
-		i++;
-	} while (!file.eof());
+int main(int argc, char** argv) {
+    if (argc == 2) {
+        Trajectory test(argv[1]);
+    }
 
-	std::cout << "walls succesfully built... " << std::endl;
-} 
-
-void Ball::KudaUpal() {
-
-
-	std::map<double, double>::iterator it1;
-	for (it1 = std::next(walls.begin()); it1 != walls.end(); ++it1) {
-		number_of_interval++;
-		if (yMove(time(it1->first, x0)) < it1->second && yMove(time(it1->first, x0)) > 0) { 
-			wall_x = it1->first; wall_y = it1->second;
-			ymove = yMove(time(it1->first, x0)); 
-			std::cout << "Ball hitted wall with x = " << it1->first << std::endl;
-			std::cout << "visota udara " << yMove(time(it1->first, x0)) << std::endl;
-
-			h0 = yMove(time(it1->first, x0)); 
-			x0 = it1->first; 
-			break; 
-		}
-
-		else if (yMove(time(it1->first, x0)) < 0)
-		{
-			std::cout << it1->first << std::endl;
-			std::cout << yMove(time(it1->first, x0)) << std::endl;
-			std::cout << " n_of_i -> " << interval.find(it1->first)->second << std::endl;
-			return;
-		}
-	}
-	
-	
-	std::map<double, double>::iterator it2 = std::next(it1);
-	while (it2 != walls.end()) {
-		if (it2->first != NULL)
-			it2 = walls.erase(it2);
-		else
-			it2++;
-	}
-
-	
- 	if (ymove < wall_y && ymove > 0) {
-		std::map<double, double>::reverse_iterator it3;
-		for (it3 = std::next(walls.rbegin()); it3 != walls.rend(); ++it3) {
-			number_of_interval--;
-			velocity.v_y = velocity.v_y - EarthGravity * time(it3->first, x0);
-			if (yMove(time(it3->first, x0)) < it3->second && yMove(time(it3->first, x0)) > 0) {
-				wall_x = it3->first; wall_y = it3->second;
-				ymove = yMove(time(it3->first, x0));
-				std::cout << "Ball hitted another wall with x = " << it3->first << std::endl;
-				std::cout << "visota udara " << yMove(time(it3->first, x0)) << std::endl;
-
-				h0 = yMove(time(it3->first, x0));
-				x0 = it3->first;
-				velocity.v_x = -velocity.v_x;
-				velocity.v_y = velocity.v_y - EarthGravity * time(it3->first, x0);
-
-				number_of_interval--;
-				break; 
-			}
-
-			else if (yMove(time(it3->first, x0)) < 0)
-			{
-				std::cout << " n_of_i <- " << interval.find(it1->first)->second << std::endl;
-				return;
-			}
-		}
-
-		
-		std::map<double, double>::reverse_iterator it4 = std::next(it3);
-		while (it4 != walls.rend()) {
-			if (it4->first != NULL)
-			{
-				walls.erase(std::next(it4).base());
-			}
-			else
-				it4++;
-		} 
-	} 
-
-	if (ymove < wall_y && ymove > 0) {
-		std::cout << " list of walls after 2nd hit: " << std::endl;
-		for (auto ii = walls.begin(); ii != walls.end(); ++ii)
-			std::cout << ii->first << " " << ii->second << std::endl;
-		KudaUpal();
-	}
-
-}
-
-int main(void) {
-	Ball ball("in.txt");
-	ball.KudaUpal();
-
-	return 0;
+    return 0;
 }
