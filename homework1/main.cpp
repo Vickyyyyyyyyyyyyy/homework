f621ce1951f7fb76b2c3d5c1162cdf3ab30e2a10
Vickyyyyyyyyyyyyy <93123748+Vickyyyyyyyyyyyyy@users.noreply.github.com>
2021-12-22 18:54:00 +0300

    Update main.cpp

--- homework1/main.cpp
+++ homework1/main.cpp
@@ -1,124 +1,79 @@
 #include <iostream>
-#include <vector>
-#include <utility>
 #include <fstream>
-#include <cmath>
 
-using namespace std;
-typedef pair <int, int> Coords;
-typedef vector <Coords> Dots;
+enum pointType { LEFTMOST, RIGHTMOST };
 
-Dots get_dots(const string& filename) {
-    Dots temp;             
-    ifstream input_file;                  
-    input_file.open(filename);   
+struct Point {
+    int x;
+    int y;
 
-    if (!input_file.is_open()) {
-        cout << "error unable to open file((" << endl;
-        return temp;
+   void printPoint(pointType type) {
+
+       if (type == LEFTMOST) {
+           std::cout << "Leftmost: " << this->x << " " << this->y << std::endl;
+       }
+
+       if (type == RIGHTMOST) {
+           std::cout << "Rightmost: " << this->x << " " << this->y << std::endl;
+       }
     }
+
+};
+
+void findPoints(Point& fr, Point& fl, const std::string& filename) {
+    std::ifstream infile(filename);
+
+    std::pair<int, int> vectorn;
+
+    double finde_value = 1;
+    double min_dist_r = 1;
+    double min_dist_l = 1;
+
+    infile >> vectorn.first >> vectorn.second;
 
     int x, y;
-    do {
-        input_file >> x >> y;
-        Coords xy_i = make_pair(x, y);
-        temp.push_back(xy_i);
-    } while (!input_file.eof());
 
-    input_file.close();
-    return temp;
-}
-
-double getCos(const Coords& src, const Coords& cmp) {
-    auto x = src.first;
-    auto y = src.second;
-
-    auto x_i = cmp.first;
-    auto y_i = cmp.second;
-
-    double first_part = (x * x_i) + (y * y_i);
-    double second_part = sqrt(x * x + y * y) * sqrt(x_i * x_i + y_i * y_i);
-
-    double result = first_part / second_part;
-
-    return result;
-}
-
-
-Dots get_result(Dots& mat) {
-    Dots result;
-    Coords left_result;
-    Coords right_result;
-    auto vect = mat[0];
-    double cos_left = 1.0;
-    double cos_right = 1.0;
-
-    while (!mat.empty()) {
-
-        Coords current_coords = mat.back();
-        mat.pop_back();
-
-        double cos = getCos(vect, current_coords);
-        if (cos == -1) {
-            right_result = current_coords;
-            cos_right = cos;
+    while (infile >> x >> y)
+    {
+        if (x == 0 && y == 0) {
             continue;
         }
-        bool isOnTheRight;
 
-        if (vect.first == 0) { 
-            if (current_coords.first > 0) {   
-                isOnTheRight = true;
-            }
-            else
-                isOnTheRight = false;
+        double cosine = (vectorn.first * x + vectorn.second * y) / (sqrt(x * x + y * y) *
+            sqrt(vectorn.first * vectorn.first + vectorn.second * vectorn.second));
+
+
+        cosine = std::round(cosine * 10000000000.0) / 10000000000.0;
+        if (cosine - min_dist_l <= 0 && (vectorn.first * y - vectorn.second * x) > 0)
+        {
+            min_dist_l = cosine;
+            fr.x = x;
+            fr.y = y;
+
         }
-        else {
-            auto x = vect.first;
-            auto y = vect.second;
-
-            auto x_i = current_coords.first;
-            auto y_i = current_coords.second;
-
-            double k = (double)y / x;
-
-            bool isXPositive = x > 0;
-            auto Y_x_i = k * x_i;
-            bool is_Y_ot_Xi_Greater_than_Yi = Y_x_i > y_i;
-
-            isOnTheRight = isXPositive ^ is_Y_ot_Xi_Greater_than_Yi;
+        if (cosine - min_dist_r <= 0 && (vectorn.first * y - vectorn.second * x) <= 0)
+        {
+            min_dist_r = cosine;
+            fl.x = x;
+            fl.y = y;
         }
-
-        if (isOnTheRight) {
-            if (cos < cos_right) {
-                cos_right = cos;
-                right_result = current_coords;
-            }
-        }
-        else {
-            if (cos < cos_left) {
-                cos_left = cos;
-                left_result = current_coords;
-            }
-        }
-
     }
-    result.push_back(left_result);
-    result.push_back(right_result);
-
-    return result;
+    infile.close();
 }
 
 
+int main() 
+{
+   
 
-int main() {
-    Dots mat = get_dots("in.txt");
+    Point fr = { 0,0 };
+    Point fl = { 0,0 };
 
-    Dots result = get_result(mat);
+    findPoints(fr, fl, "in.txt");
 
-    cout << "Leftmost:\t" << result[0].first << " " << result[0].second << endl;
-    cout << "Rightmost:\t" << result[1].first << " " << result[1].second << endl;
+    //Печатаем при помощи функции void printPoint из struct Point
+    fr.printPoint(LEFTMOST);
+    fl.printPoint(RIGHTMOST);
 
     return 0;
 }
-
